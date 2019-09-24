04ad4f97b25c479dc2ffba9dcbcbd25a8eab757f
Ryan Keys <r.keys1998@gmail.com>
2019-09-24 15:25:28 -0700

    enabled fight fns and fixed bugs with prior fns

--- superheroes.py
+++ superheroes.py
@@ -2,17 +2,16 @@ import random
 
 
 class Ability:
-    def __init__(self, name, max_dmg):
+    def __init__(self, name, attack_power):
         '''Create Instance Variables:
         name: str
         max_damage: int'''
         self.name = name
-        self.max_dmg = max_dmg
+        self.max_dmg = attack_power
 
     def attack(self):
         ''' Return a value between 0 and the value set by self.max_damage.'''
-        dmg = random.randint(0, self.max_dmg)
-        return dmg
+        return random.randint(0, self.max_dmg)
 
 
 class Armor:
@@ -25,8 +24,7 @@ class Armor:
         self.max_block = max_block
 
     def block(self):
-        block = random.randint(0, self.max_block)
-        return block
+        return random.randint(0, self.max_block)
 
 
 class Hero:
@@ -46,32 +44,71 @@ class Hero:
     def add_ability(self, ability):
         '''add ability to list'''
         self.abilities.append(ability)
-        return self.abilities
 
     def attack(self):
         '''Calculate the total damage from all ability attacks.
           return: total:Int'''
-
-        for hit in self.abilities:
-            return Ability.attack(hit)
+        count = 0
+        for ability in self.abilities:
+            count += ability.attack()
+            return count
 
     def add_armor(self, armor):
         '''Add armor to self.armors Armor: Armor Object'''
-        for armor in self.armors:
-            return armor + self.current_health
+        self.armors.append(armor)
 
     def defend(self, dmg_amt):
+        count = 0
         for armor in self.armors:
-            return dmg_amt - armor
+            block_int = int(armor.block())
+            count += block_int
+        return count + dmg_amt
 
     def take_dmg(self, damage):
         '''Updates self.current_health to reflect the damage minus the defense.'''
-        return self.current_health - damage
+        hp = self.current_health
+        defense = self.defend(damage)
+        self.current_health = hp - defense
+
+    def is_alive(self):
+        if self.current_health < 0:
+            print(f"{self.name} is dead!")
+            return False
+        else:
+            return True
+        pass
+
+    def fight(self, opponent):
+        '''Current Hero will take turns fighting the opponent hero passed in.'''
+        attacking = True
+        while attacking:
+            if self.is_alive():
+                dmg = self.attack()
+                opponent.take_dmg(dmg)
+                print(f"{self.name} has {self.current_health} health left.")
+
+            else:
+                print(f"{self} lost")
+                break
+
+            if opponent.is_alive():
+                enemy_dmg = opponent.attack()
+                self.take_dmg(enemy_dmg)
+                print(f"{opponent.name} has {opponent.current_health} health left.")
+
+            else:
+                break
 
 
 if __name__ == "__main__":
-    hero = Hero("Grace Hopper", 200)
-    shield = Armor("Shield", 50)
-    hero.add_armor(shield)
-    hero.take_dmg(50)
-    print(hero.current_health)
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
